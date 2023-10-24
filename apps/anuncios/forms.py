49908f37d5a67d3345f9617ca7f211f9e50643b3
dennys <dennyscorrea58@gmail.com>
2023-10-24 14:03:31 -0400

    .

--- apps/anuncios/forms.py
+++ apps/anuncios/forms.py
@@ -1,8 +1,51 @@
 from django import forms
 from .models import Usuario
+from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
 class PersonalForm(forms.ModelForm):
     class Meta:
         model = Usuario
         fields = '__all__'
+
+class UserForm(forms.ModelForm):
+    password1 = forms.CharField(label='Contrasena', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm Contrasena', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2', 'is_active']
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Las contrasena no coinciden")
+        return password2
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("El email ya está en uso.")
+        return email
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+class UserFormEdit(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name', 'is_active']
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        username = self.cleaned_data.get("username")
+        if User.objects.exclude(username=username).filter(email=email).exists():
+            raise forms.ValidationError("El email ya está en uso.")
+        return email
--- apps/anuncios/views/personal/views.py
+++ apps/anuncios/views/personal/views.py
@@ -1,32 +1,62 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
-from ...forms import PersonalForm
+from ...forms import PersonalForm, UserForm
 from django.views.generic import (
-    TemplateView,
-    ListView
+	TemplateView,
+	ListView
 )
 from ...models import Usuario
+from django.contrib.auth.models import User
+
 
 class ListadoPersonal(LoginRequiredMixin, ListView):
-    context_object_name = 'personal_list'
-    template_name = 'pages/personal/listado_personal.html'
-    ordering = ['nombre']
+	context_object_name = 'personal_list'
+	template_name = 'pages/personal/listado_personal.html'
+	ordering = ['nombre']
 
-    def get_queryset(self):
-        return Usuario.objects.exclude(tipo_usuario=Usuario.Permission.CLIENTE)
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["title"] = "Personal"
-        context["sub_title"] = "Listado de personal"
-        return context
+	def get_queryset(self):
+		return Usuario.objects.exclude(tipo_usuario=Usuario.Permission.CLIENTE)
+	
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context["title"] = "Personal"
+		context["sub_title"] = "Listado de personal"
+		return context
 
 class RegistrarPersonal(LoginRequiredMixin, TemplateView):
-    template_name = 'pages/personal/registrar_personal.html'
+	template_name = 'pages/personal/registrar_personal.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["title"] = "Personal"
-        context["sub_title"] = "Registrar personal"
-        context["form"] = PersonalForm()
-        return context
+	def post(self, request, *args, **kwargs):
+		try:
+			form = UserForm(request.POST)
+			if form.is_valid():
+				if not User.objects.filter(username = form.cleaned_data['cedula']).exists():
+					form.save()
+					
+					user = User.objects.get(username = form.cleaned_data['cedula'])
+					# permissions = Permission.objects.filter(codename__in=group_permissions_choices[form.cleaned_data['role']])
+					# for permission in permissions:
+					# 	user.user_permissions.add(permission)
+					# user.save()
+
+					rol = UserSecurity()
+					rol.workstation = form.cleaned_data['role']
+					rol.user_id = user.pk
+					rol.save()
+					
+				else:
+					data['response'] = {'title':'Ocurrió un error!', 'data': 'El usuario ya esta registrado', 'type_response': 'danger'}
+			else:
+				for field, errors in form.errors.items():
+					for error in errors:
+						data['response'] = {'title':field, 'data': error, 'type_response': 'danger'}
+		except Exception as e:
+			data['error'] = str(e)
+		return JsonRresponse(data, safe=False)
+							
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context["title"] = "Personal"
+		context["sub_title"] = "Registrar personal"
+		context["form"] = PersonalForm()
+		return context
