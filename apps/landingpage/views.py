4d0dfc9036e4aeb337f04f5a7ea63760f6d79c5f
dennys <dennyscorrea58@gmail.com>
2023-11-04 13:15:47 -0400

    pantalla de mis citas

--- apps/landingpage/views.py
+++ apps/landingpage/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.views.generic import (
+	UpdateView,
+	ListView,
+	CreateView,
+	DetailView,
+	View,
+    TemplateView
+)
 
-# Create your views here.
+class Inicio(TemplateView):
+    template_name = 'landingpage/pages/inicio.html'
+    
+class MisCitas(TemplateView):
+    template_name = 'landingpage/pages/mis_citas.html'
+
+class SolicitarCita(TemplateView):
+    template_name = 'landingpage/pages/registrar_cita.html'
+
+class DetalleMiCita(TemplateView):
+    template_name = 'landingpage/pages/registrar_cita.html'
