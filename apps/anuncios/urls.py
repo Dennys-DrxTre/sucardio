40eb81758c99c95e5c3d092842ac545ed1c41047
Jose David <josedaviddurand@gmail.com>
2023-11-05 15:48:38 -0400

    anuncios

--- apps/anuncios/urls.py
+++ apps/anuncios/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ListadoAnuncios, ListadoAnuncioAdmin, RegistrarAnuncio
+from .views import ListadoAnuncios, ListadoAnuncioAdmin, RegistrarAnuncio, EditarAnuncio, CambiarEstadoAnuncio, DetalleAnuncio, DetalleAnuncioLanding
 
 APP_NAME = 'anuncio'
 
@@ -7,4 +7,8 @@ urlpatterns = [
     path('listado-de-anuncios/', ListadoAnuncioAdmin.as_view(), name='listado_anuncios_admin' ),
     path('anuncios/', ListadoAnuncios.as_view(), name='listado_anuncios' ),
     path('registrar-anuncio/', RegistrarAnuncio.as_view(), name='registrar_anuncio' ),
+    path('editar-anuncio/<int:pk>/', EditarAnuncio.as_view(), name='editar_anuncio' ),
+    path('detalles-del-anuncio/<int:pk>/', DetalleAnuncio.as_view(), name='detalle_anuncio' ),
+    path('anuncio/<int:pk>/', DetalleAnuncioLanding.as_view(), name='detalle_anuncio_landing' ),
+    path('cambiar-estado-anuncio/<int:pk>/', CambiarEstadoAnuncio.as_view(), name='cambiar_estado_anuncio' ),
 ]
