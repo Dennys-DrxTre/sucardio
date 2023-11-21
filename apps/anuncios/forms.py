83353d9dedec8f522c42dadae607f3caa381ada0
dennys <dennyscorrea58@gmail.com>
2023-11-20 19:24:37 -0500

    filtrar autores que no sean clientes y fecha de editar cita

--- apps/anuncios/forms.py
+++ apps/anuncios/forms.py
@@ -1,7 +1,8 @@
 from django import forms
-from .models import Anuncios
+from .models import Anuncios, Usuario
 
 class AnunciosForm(forms.ModelForm):
+	autor = forms.ModelChoiceField(queryset=Usuario.objects.exclude(tipo_usuario=Usuario.Permission.CLIENTE).filter(user__is_active=True,user__is_superuser=False))
 	class Meta:
 		model = Anuncios
 		fields = '__all__'
